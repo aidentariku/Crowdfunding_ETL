52e208f1eca0480b017d7479dd48a19dfa49b055
karl-schultz <karl.philip.schultz@gmail.com>
2023-12-13 19:44:14 -0800

    cleaning up repository

--- Resources/crowdfunding_db_schema.sql
+++ Resources/crowdfunding_db_schema.sql
@@ -1,34 +1,31 @@
-CREATE TABLE campaign (
-    cf_id int   NOT NULL,
-    contact_id int   NOT NULL,
-    company_name varchar(100) NOT NULL,
-    description text NOT NULL,
-    goal int NOT NULL,
-    pledged int NOT NULL,
-    outcome varchar(50) NOT NULL,
-    backers_count int NOT NULL,
-    country varchar(10) NOT NULL,
-    currency varchar(10) NOT NULL,
-    launch_date date NOT NULL,
-    end_date date NOT NULL,
-    category_id varchar(10)   NOT NULL,
-    subcategory_id varchar(10)   NOT NULL,
-    CONSTRAINT pk_campaign PRIMARY KEY (
-        cf_id
-     )
+CREATE TABLE campaigns (
+    cf_id serial PRIMARY KEY,
+    contact_id VARCHAR,
+    company_name VARCHAR(100),
+    description TEXT,
+    goal VARCHAR,
+    pledged VARCHAR,
+    outcome VARCHAR,
+    backers_count VARCHAR,
+    country VARCHAR,
+    currency VARCHAR,
+    launched_date DATE,
+    end_date DATE,
+    category_id VARCHAR,
+    subcategory_id VARCHAR
 );
 
 CREATE TABLE category (
-    category_id varchar(10) NOT NULL,
-    category_name varchar(50) NOT NULL,
+    category_id VARCHAR(10) NOT NULL,
+    category_name VARCHAR(50) NOT NULL,
     CONSTRAINT pk_category PRIMARY KEY (
         category_id
      )
 );
 
 CREATE TABLE subcategory (
-    subcategory_id varchar(10) NOT NULL,
-    subcategory_name varchar(50) NOT NULL,
+    subcategory_id VARCHAR(10) NOT NULL,
+    subcategory_name VARCHAR(50) NOT NULL,
     CONSTRAINT pk_subcategory PRIMARY KEY (
         subcategory_id
      )
@@ -36,9 +33,9 @@ CREATE TABLE subcategory (
 
 CREATE TABLE contacts (
     contact_id int NOT NULL,
-    first_name varchar(50) NOT NULL,
-    last_name varchar(50) NOT NULL,
-    email varchar(100) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
     CONSTRAINT pk_contacts PRIMARY KEY (
         contact_id
      )
